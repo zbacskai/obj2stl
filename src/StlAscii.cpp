52375f1029e6ac1d9e3e9927b03d2b4f7b23068e
Zoltan Bacskai <z.bacskai.jr@gmail.com>
2020-09-29 21:21:42 +0100

    Conversion for stl-ascii

--- src/StlAscii.cpp
+++ src/StlAscii.cpp
@@ -7,6 +7,40 @@
 
 #include <arpa/inet.h>
 
+namespace {
+
+    void writeVertex(const trim::Triangle& triangle, 
+                           std::ofstream& ofile)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            ofile << "        vertex ";
+            ofile << triangle[i].p[0] << " ";
+            ofile << triangle[i].p[1] << " ";
+            ofile << triangle[i].p[2] << std::endl;
+        }
+    }
+
+    void writeNormalVector(const trim::Triangle& triangle, 
+                           std::ofstream& ofile)
+    {
+        ofile << triangle.getNormalVector()[0] << " ";
+        ofile << triangle.getNormalVector()[1] << " ";
+        ofile << triangle.getNormalVector()[2] << std::endl;
+    }
+
+    void writeTriangle(const trim::Triangle& triangle, 
+                       std::ofstream& ofile)
+    {
+        ofile << "facet normal ";
+        writeNormalVector(triangle, ofile);
+        ofile << "    outer loop" << std::endl;
+        writeVertex(triangle, ofile);
+        ofile << "    endloop" << std::endl;
+        ofile << "endfacet" << std::endl;
+    }
+}
+
 namespace stl {
 namespace ascii {
 
@@ -17,30 +51,13 @@ StlAscii::StlAscii(const char* fileName) : meshconvert::FileWriterInterface(file
 void StlAscii::write(const trim::TriangleModel &tm) {
     std::ofstream ofile;
     ofile.open (fileName_);
-  
-    ofile << "solid test " << std::endl;
     ofile << std::setprecision(6);
     ofile << std::scientific;
 
+    ofile << "solid test " << std::endl;
+
     for (auto triangle : tm.getTriangles())
-    {
-        ofile << "facet normal ";
-        ofile << triangle->getNormalVector().p[0] << " ";
-        ofile << triangle->getNormalVector().p[1] << " ";
-        ofile << triangle->getNormalVector().p[2] << std::endl;
-
-        ofile << "    outer loop" << std::endl;
-        for (int i = 0; i < 3; ++i)
-        {
-            ofile << "        vertex ";
-            ofile << triangle->operator[](i).p[0] << " ";
-            ofile << triangle->operator[](i).p[1] << " ";
-            ofile << triangle->operator[](i).p[2] << std::endl;
-
-        }
-        ofile << "    endloop" << std::endl;
-        ofile << "endfacet" << std::endl;
-    }
+        writeTriangle(*triangle, ofile);
     ofile.close();
 }
 
